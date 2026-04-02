de2a06e1fc3456e55c70b42468df2844ce8a61eb
Ben Parks <benparks32@gmail.com>
2026-04-02 16:50:41 +0100

    Fix HomeMode color encoding pipeline for rings and touch pills

--- src/Modes/HomeMode.cpp
+++ src/Modes/HomeMode.cpp
@@ -2,15 +2,20 @@
 
 namespace
 {
-static uint16_t rgb888To565(uint32_t rgb)
+static uint32_t rgb565To888(uint16_t rgb565)
 {
-    const uint8_t r = (uint8_t)((rgb >> 16) & 0xFFU);
-    const uint8_t g = (uint8_t)((rgb >> 8) & 0xFFU);
-    const uint8_t b = (uint8_t)(rgb & 0xFFU);
-    return (uint16_t)(((uint16_t)(r & 0xF8U) << 8) | ((uint16_t)(g & 0xFCU) << 3) | ((uint16_t)b >> 3));
+    const uint8_t r5 = (uint8_t)((rgb565 >> 11) & 0x1FU);
+    const uint8_t g6 = (uint8_t)((rgb565 >> 5) & 0x3FU);
+    const uint8_t b5 = (uint8_t)(rgb565 & 0x1FU);
+
+    const uint8_t r8 = (uint8_t)(((uint16_t)r5 * 255U + 15U) / 31U);
+    const uint8_t g8 = (uint8_t)(((uint16_t)g6 * 255U + 31U) / 63U);
+    const uint8_t b8 = (uint8_t)(((uint16_t)b5 * 255U + 15U) / 31U);
+
+    return ((uint32_t)r8 << 16) | ((uint32_t)g8 << 8) | b8;
 }
 
-void applyButtonVisualFromRing(FootSwitchTouchButton& button, uint32_t ringColour, uint8_t ringBrightness)
+void applyButtonVisualFromFunctionColour(FootSwitchTouchButton& button, uint16_t colour565, uint8_t ringBrightness)
 {
     if (ringBrightness == 0)
     {
@@ -19,7 +24,7 @@ void applyButtonVisualFromRing(FootSwitchTouchButton& button, uint32_t ringColou
         return;
     }
 
-    button.setPillColour(rgb888To565(ringColour));
+    button.setPillColour(colour565);
     button.setBorderVisible(ringBrightness >= RingManager::FullBrightness);
 }
 } // namespace
@@ -34,20 +39,20 @@ HomeMode::HomeMode(TouchButtonManager& touchButtonManager, RingManager& ringMana
 void HomeMode::setupFunctions()
 {
     // Button 0: Amp (Green) - MIDI Program 1
-    _functions[0] = Function("Amp", 0x00FF00, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[0] = Function("Amp", 0x07E0, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
 
     // Button 1: Ampless (Blue) - MIDI Program 6
-    _functions[1] = Function("Ampless", 0x0000FF, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[1] = Function("Ampless", 0x001F, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
 
     // Button 2: CodeRed (Red) - MIDI Program 20
-    _functions[2] = Function("CodeRed", 0xFF0000, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[2] = Function("CodeRed", 0xF800, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
 
     // Buttons 3-7: Additional preset slots (disabled for now)
-    _functions[3] = Function(" ", 0x0000FF, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
-    _functions[4] = Function(" ", 0xDCA500, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
-    _functions[5] = Function(" ", 0x008080, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
-    _functions[6] = Function(" ", 0xFF80FF, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
-    _functions[7] = Function(" ", 0xFFFFFF, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[3] = Function(" ", 0x001F, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[4] = Function(" ", 0xDD20, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[5] = Function(" ", 0x0410, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[6] = Function(" ", 0xFC1F, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
+    _functions[7] = Function(" ", 0xFFFF, ActionType::SendMidiProgramChange, ActionType::SendMidiProgramChange);
 }
 
 void HomeMode::activate()
@@ -61,16 +66,18 @@ void HomeMode::activate()
         }
 
         const Function& func = getFunction(i);
+        const uint16_t colour565 = func.colour();
+        const uint32_t ringColour888 = rgb565To888(colour565);
         const bool enabled = isButtonEnabled(i);
         const uint8_t ringBrightness = enabled ? RingManager::FullBrightness : 0;
 
         button->setEnabled(enabled);
         button->setLabel(func.label());
-        applyButtonVisualFromRing(*button, func.colour(), ringBrightness);
+        applyButtonVisualFromFunctionColour(*button, colour565, ringBrightness);
 
         if (enabled)
         {
-            _ringManager.setRingColour(i, func.colour());
+            _ringManager.setRingColour(i, ringColour888);
             _ringManager.setRingBrightness(i, ringBrightness);
         }
         else
