a028bc8996e53e74e9d95bce6eb897c953118064
Ben Parks <benparks32@gmail.com>
2026-04-03 10:48:09 +0100

    Refactor HomeMode patch values into named constants

--- src/Modes/HomeMode.cpp
+++ src/Modes/HomeMode.cpp
@@ -1,5 +1,9 @@
 #include "Modes/HomeMode.h"
 
+const uint8_t STOMP_PATCH_AMP = 0;
+const uint8_t STOMP_PATCH_AMPLESS = 6;
+const uint8_t STOMP_PATCH_CODERED = 20;
+
 HomeMode::HomeMode(TouchButtonManager& touchButtonManager, RingManager& ringManager, ScreenUi& screenUi,
                    IMidiManager& midiManager, IModeTransistionDelegate& transitionDelegate)
     : FunctionModeBase(touchButtonManager, ringManager, screenUi, midiManager, transitionDelegate)
@@ -10,13 +14,13 @@ HomeMode::HomeMode(TouchButtonManager& touchButtonManager, RingManager& ringMana
 void HomeMode::setupFunctions()
 {
     // Button 0: Amp (Green) -> enter Play and select Home Program 0
-    _functions[0] = Function("Amp", 0x07E0, ActionType::ChangeMode, 0, ActionType::None, 0);
+    _functions[0] = Function("Amp", 0x07E0, ActionType::ChangeMode, STOMP_PATCH_AMP, ActionType::None, 0);
 
     // Button 1: Ampless (Blue) -> enter Play and select Home Program 6
-    _functions[1] = Function("Ampless", 0x001F, ActionType::ChangeMode, 6, ActionType::None, 0);
+    _functions[1] = Function("Ampless", 0x001F, ActionType::ChangeMode, STOMP_PATCH_AMPLESS, ActionType::None, 0);
 
     // Button 2: CodeRed (Red) -> enter Play and select Home Program 20
-    _functions[2] = Function("CodeRed", 0xF800, ActionType::ChangeMode, 20, ActionType::None, 0);
+    _functions[2] = Function("CodeRed", 0xF800, ActionType::ChangeMode, STOMP_PATCH_CODERED, ActionType::None, 0);
 
     // Buttons 3-7: Disabled slots use Function default constructor values.
     _functions[3] = Function();
